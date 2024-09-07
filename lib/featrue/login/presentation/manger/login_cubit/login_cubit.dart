be7165ee5c1b70c83f3518758ceac3c0de8ba115
Mohamed Soliman <ms3196@fayoum.edu.eg>
2024-09-07 03:32:42 +0300

    - presentation layer , create login cubit

---
+++ lib/featrue/login/presentation/manger/login_cubit/login_cubit.dart
@@ -0,0 +1,42 @@
+import 'package:car_wash_app/featrue/login/domain/entitys/login_entity.dart';
+import 'package:car_wash_app/featrue/login/domain/use_cases/login_use_case.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit(this.loginUseCase) : super(LoginInitial());
+  final LoginUseCase loginUseCase;
+
+  //? form key
+  final formKey = GlobalKey<FormState>();
+  //? auto validate
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+  //? on save
+  String? email, password;
+
+  //? login
+  Future<void> login() async {
+    emit(LoginLoading());
+    var result = await loginUseCase.call(
+      LoginEntity(email: email!, password: password!),
+    );
+    //
+    result.fold(
+      (failuer) => emit(LoginFailed(errMessage: failuer.errMessage)),
+      (success) => emit(LoginSuccess()),
+    );
+  }
+
+  //? validate title and content and add note
+  Future<void> validate() async {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      await login();
+    } else {
+      autovalidateMode = AutovalidateMode.always;
+      emit(ValidateState());
+    }
+  }
+}
---
+++ lib/featrue/login/presentation/manger/login_cubit/login_state.dart
@@ -0,0 +1,18 @@
+part of 'login_cubit.dart';
+
+sealed class LoginState {}
+
+final class LoginInitial extends LoginState {}
+
+final class LoginLoading extends LoginState {}
+
+final class LoginSuccess extends LoginState {}
+
+final class LoginFailed extends LoginState {
+  final String errMessage;
+
+  LoginFailed({required this.errMessage});
+}
+
+
+final class ValidateState extends LoginState {}
